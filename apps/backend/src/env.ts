9e7aa020fb5152c93c4cb5a6352ae027c08791b4
bartbie <bartbie37@gmail.com>
2024-12-19 00:36:20 +0100

    backend: added env

---
+++ apps/backend/src/env.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+import z from 'zod';
+
+dotenv.config();
+
+const ENV_SCHEMA = z.object({
+  PORT: z.coerce.number().int().positive().default(3000),
+  MONGO_URL: z.string().url().min(1),
+  AUTH_SECRET: z.string().min(64),
+  // https://github.com/vercel/ms
+  TOKEN_AGE: z.string().min(1).default('30m'),
+  NODE_ENV: z
+    .preprocess(
+      (x) => typeof x == 'string' && x.toLowerCase(),
+      z.enum(['development', 'production']),
+    )
+    .default('production'),
+});
+
+const parsed = ENV_SCHEMA.safeParse(process.env);
+
+if (parsed.success === false) {
+  console.error(
+    '❌[build] Invalid environment variables:',
+    parsed.error.flatten().fieldErrors,
+  );
+  throw new Error('Invalid environment variables');
+}
+
+export const env = parsed.data;
