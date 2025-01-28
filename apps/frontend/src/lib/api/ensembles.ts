29853a6e738980bb4a7db45189a39e00ec3a3b73
bartbie <bartbie37@gmail.com>
2025-01-28 03:07:31 +0100

    Fixes wrong ensembles api url

--- apps/frontend/src/lib/api/ensembles.ts
+++ apps/frontend/src/lib/api/ensembles.ts
@@ -5,35 +5,35 @@ import { api } from "./_client";
 type AResult<T, E = string> = Promise<Result<T, E>>;
 
 export const getAll = async (): AResult<API.EnsembleFull[], never> => {
-    return await api.get(`/ensemble`);
+    return await api.get(`/ensembles`);
 };
 
 export const get = async (id: string): AResult<API.EnsembleFull> => {
-    return await api.get(`/ensemble/${id}`);
+    return await api.get(`/ensembles/${id}`);
 };
 
 export const join = async (id: string): AResult<API.EnsembleFull> => {
-    return await api.post(`/ensemble/${id}/join`);
+    return await api.post(`/ensembles/${id}/join`);
 };
 
 export const leave = async (id: string): AResult<API.EnsembleFull> => {
-    return await api.post(`/ensemble/${id}/leave`);
+    return await api.post(`/ensembles/${id}/leave`);
 };
 
 export const disband = async (id: string): AResult<void> => {
-    return await api.delete(`/ensemble/${id}`);
+    return await api.delete(`/ensembles/${id}`);
 };
 
 export const create = async (
     id: string,
     data: API.NewEnsemble,
 ): AResult<API.Ensemble> => {
-    return await api.post(`/ensemble/${id}`, data);
+    return await api.post(`/ensembles/${id}`, data);
 };
 
 export const edit = async (
     id: string,
     data: API.UpdateEnsemble,
 ): AResult<API.EnsembleFull> => {
-    return await api.patch(`/ensemble/${id}`, data);
+    return await api.patch(`/ensembles/${id}`, data);
 };
